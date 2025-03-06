c0e734d5272f37a5b8342fa2e7579c985ae99709
monsterarty <masleanco@gmail.com>
2025-03-06 17:41:10 +0100

    13: Added Markdown_blocks and its unittests... terrible

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,6 +1,10 @@
 import re
 from enum import Enum
 
+from htmlnode import ParentNode
+from textnode import TextType, TextNode, text_node_to_html_node
+from inline_markdown import text_to_textnodes
+
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
     HEADING = "heading"
@@ -9,11 +13,98 @@ class BlockType(Enum):
     ULIST = "unordered_list"
     OLIST = "ordered_list"
 
+def markdown_to_html_node(markdown):
+
+    parent = ParentNode("div", [])
+    child_list = []
+    blocks = markdown_to_blocks(markdown)
+
+    for block in blocks:
+        match block_to_block_type(block):
+            case BlockType.PARAGRAPH:
+                paragraph_text = block.replace("\n", " ")
+                child = ParentNode("p", text_to_children(paragraph_text))
+                child_list.append(child)
+            case BlockType.HEADING:
+                level = hashtag_level(block)
+                content = block.lstrip("#").strip()
+                child = ParentNode(f"h{level}", text_to_children(content))
+                child_list.append(child)
+            case BlockType.CODE:
+                lines = block.split("\n")
+                code_content = "\n".join(lines[1:-1]) + "\n"
+
+                text_node = TextNode(code_content, TextType.TEXT)
+                html_node = text_node_to_html_node(text_node)
+                
+                code_node = ParentNode("code", [html_node])
+                pre_node = ParentNode("pre", [code_node])
+                child_list.append(pre_node)
+            case BlockType.QUOTE:
+                clean_lines = []
+                for line in block.split("\n"):
+                    if line.startswith(">"):
+                        clean_lines.append(line.lstrip(">").strip())
+                    else:
+                        clean_lines.append(line.strip())
+                clean_content = "\n".join(clean_lines)
+                
+                child = ParentNode("blockquote", text_to_children(clean_content))
+                parent.children.append(child)
+            case BlockType.ULIST:
+                lines = split_lines(block, BlockType.ULIST)
+                child = ParentNode("ul", lines)
+                child_list.append(child)
+            case BlockType.OLIST:
+                lines = split_lines(block, BlockType.OLIST)
+                child = ParentNode("ol", lines)
+                child_list.append(child) 
+            case _:
+                raise Exception("Invalid block type")
+                
+    parent.children.extend(child_list)
+    return parent
+
+def split_lines(block, block_type):
+    lines = block.split("\n")
+    children = []
+    for line in lines:
+        if not line.strip():
+            continue
+            
+        if block_type == BlockType.ULIST:
+            content = line.lstrip("- ").strip()
+        elif block_type == BlockType.OLIST:
+            content = re.sub(r"^\d+\.\s*", "", line).strip()
+        else:
+            content = line.strip()
+            
+        # Create li element with processed content
+        li_node = ParentNode("li", text_to_children(content))
+        children.append(li_node)
+        
+    return children
+
+def hashtag_level(hashtag):
+    count = 0
+    for char in hashtag:
+        if char == "#":
+            count += 1
+        else:
+            break
+
+    return min(max(count, 1), 6)
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    html_nodes = []
+    for node in text_nodes:
+        html_nodes.append(text_node_to_html_node(node))
+    return html_nodes
+
 def block_to_block_type(block):
     lines = block.split("\n")
 
-    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
-        return BlockType.HEADING
     if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
         return BlockType.CODE
     if block.startswith(">"):
@@ -33,6 +124,8 @@ def block_to_block_type(block):
                 return BlockType.PARAGRAPH
             i += 1
         return BlockType.OLIST
+    if block.startswith(("#", "##", "###", "####", "#####", "######")):
+        return BlockType.HEADING
     return BlockType.PARAGRAPH
 
 def markdown_to_blocks(markdown):
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -2,7 +2,8 @@ import unittest
 from markdown_blocks import (
     BlockType, 
     markdown_to_blocks, 
-    block_to_block_type
+    block_to_block_type,
+    markdown_to_html_node
 )
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -117,6 +118,39 @@ class TestBlockToBlockType(unittest.TestCase):
         heading_with_spaces = "   # Heading with spaces"
         self.assertEqual(block_to_block_type(heading_with_spaces), BlockType.PARAGRAPH)
 
+        
+class TestBlockToHtml(unittest.TestCase):
+    def test_paragraphs(self):
+        md = """
+    This is **bolded** paragraph
+    text in a p
+    tag here
+
+    This is another paragraph with _italic_ text and `code` here
+
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+    ```
+    This is text that _should_ remain
+    the **same** even with inline stuff
+    ```
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
 
 
 if __name__ == '__main__':
